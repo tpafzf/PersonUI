25656bbd1c489e7860b63f104078c9401c3a0cdb
Travis Agne <agnet16@students.bths201.org>
2019-09-11 14:48:31 -0500

    Initial Commit

---
+++ PersonUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonUI
+//
+//  Created by Travis Agne on 9/11/19.
+//  Copyright © 2019 Travis Agne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
