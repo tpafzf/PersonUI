bcff69007631b0b74379bac03e76dc62a5f4b38c
Travis Agne <travis@Traviss-MacBook-Pro.local>
2019-09-12 20:20:14 -0500

    finished the homework
    c

--- PersonUI/ViewController.swift
+++ PersonUI/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func onSlide(_ sender: Any) {
+        var intValue = Int(slider.value)
+        ageLabel.text = String(intValue)
+    }
+    
 }
 
